1b9c4ed701d75c0180ed3303eb014532402df842
Bros-flame <40359316+Bros-flame@users.noreply.github.com>
2022-06-13 12:06:04 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,74 @@
+/*!
+    * Start Bootstrap - Resume v6.0.0 (https://startbootstrap.com/template-overviews/resume)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-resume/blob/master/LICENSE)
+    */
+    (function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#sideNav",
+    });
+})(jQuery); // End of use strict
+
+
+
+
+
+
+
+
+//Skills data
+
+function loadSkillgraph() {
+$(".skillData").each(function(index, element) {
+    // element == this
+    var mydata = $(element).data();
+    var cnt = 0;
+
+    //recursive call with a time delay so user can see graph draw.
+    function go() {
+        if (cnt++ < mydata['percent']) {
+            setTimeout(go, 10);
+        }
+        $(element).css('width', cnt + '%');
+
+    }
+
+    go();
+
+});
+
+}
+
+loadSkillgraph();
